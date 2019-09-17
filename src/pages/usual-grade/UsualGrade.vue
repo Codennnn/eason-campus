afe4b6c115499ac55f3bc5b9a66894b6cf9119fa
令狐少侠 <czc12580520@gmail.com>
2019-09-18 00:42:50 +0800

    🚀 代码优化

--- src/pages/usual-grade/UsualGrade.vue
+++ src/pages/usual-grade/UsualGrade.vue
@@ -1,20 +1,147 @@
 <template>
-  <div>
+  <div class="main">
+    <div class="card bg-white">
+      <div class="flex align-center justify-between">
+        <div>课程名称</div>
+        <div>分数</div>
+      </div>
+      <template v-if="!loading && itmes.length > 0">
+        <div
+          v-for="(item, key) in itmes"
+          :key="key"
+        >
+          <div
+            class="flex align-center justify-between mt-1 p-1"
+            @click="showItems(key)"
+          >
+            <div>{{ item.name }}</div>
+            <div
+              class="primary"
+              style="font-weight: bold;font-size: 28rpx;"
+            >{{ item.total || '--' }}</div>
+          </div>
+          <div
+            class="items"
+            style="border-radius: 10rpx;background: #ececec;"
+            :class="currentShow === key ? 'show my-1 p-2' : 'hide'"
+          >
+            <template v-if="currentShow === key">
+              <div
+                class="grid text-center"
+                v-for="(it, i) in item.items"
+                :key="i"
+              >
+                <span class="label">{{ it.label }}</span>
+                <span>{{ it.percentage }}</span>
+                <span>{{ it.most }}</span>
+                <span>{{ it.score }}</span>
+              </div>
+            </template>
+          </div>
+        </div>
+      </template>
+      <div
+        style="height: 500rpx;"
+        v-if="loading"
+      >
+        <LoaderCircle />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getUsualGrades } from '@/request/api'
+import LoaderCircle from '@/components/LoaderCircle'
+
 export default {
   data () {
     return {
+      itmes: [],
+      currentShow: null,
+      loading: false
     }
   },
 
+  components: { LoaderCircle },
+
+  mounted () {
+    this.getUsualGrades()
+  },
+
   methods: {
+    async getUsualGrades () {
+      try {
+        this.loading = true
+        const account = mpvue.getStorageSync('user')
+        const { code, data } = await getUsualGrades(account)
+        if (code === 1000) {
+          this.itmes = data.usual_grades
+        }
+      } catch (err) {
+        const that = this
+        mpvue.showModal({
+          content: '呜呜呜，服务器罢工了',
+          confirmText: '再试一下',
+          confirmColor: '#5d97f7',
+          success ({ confirm, cancel }) {
+            if (confirm) {
+              that.getUsualGrades()
+            } else if (cancel) {
+              mpvue.navigateBack()
+            }
+          }
+        })
+      } finally {
+        this.loading = false
+      }
+    },
+
+    showItems (i) {
+      if (this.currentShow === i) {
+        this.currentShow = -1
+        return
+      }
+      this.currentShow = i
+    }
   }
 
 }
 </script>
 
 <style scoped>
+.main {
+  height: 100vh;
+  padding: 40rpx 40rpx;
+}
+
+.card {
+  padding: 20rpx;
+  border-radius: 10rpx;
+  box-shadow: 0 0 20rpx #ececec;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1.5fr 1fr 1fr 1fr;
+}
+
+.label {
+  word-wrap: break-word;
+  word-break: break-all;
+}
+
+.items {
+  transition: all 0.3s;
+}
+
+.show {
+  opacity: 1;
+  height: auto;
+}
+
+.hide {
+  opacity: 0;
+  height: 0;
+}
 </style>
--- src/request/api.js
+++ src/request/api.js
@@ -2,28 +2,39 @@ import request from './request'
 
 const host = 'http://localhost:7001'
 
+// 登录
 export async function login (data) {
   const res = await request.post(`${host}/login`, data)
   return res
 }
 
+// 学生信息
 export async function getStudentInfo (data) {
   const res = await request.post(`${host}/info`, data)
   return res
 }
 
+// 课表信息
 export async function getSchedule (data) {
   const res = await request.post(`${host}/schedule`, data)
   return res
 }
 
+// 当前学期的成绩
 export async function getCurrentGrades (data) {
   data.current_term = '2019年第一学期'
   const res = await request.post(`${host}/current-grades`, data)
   return res
 }
 
+// 全部学期的成绩
 export async function getAllGrades (data) {
   const res = await request.post(`${host}/all-grades`, data)
+  return res
+}
+
+// 平时成绩
+export async function getUsualGrades (data) {
+  const res = await request.post(`${host}/usual-grades`, data)
   return res
 }
