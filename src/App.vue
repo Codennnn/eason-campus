388886fa00aa63626b735785230da2fbb5869552
令狐少侠 <czc12580520@gmail.com>
2019-12-11 13:39:24 +0800

    🎨 样式调整

--- src/App.vue
+++ src/App.vue
@@ -36,12 +36,4 @@ export default {
   font-size: 30rpx;
   background: #f9f7fa;
 }
-
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
 </style>
